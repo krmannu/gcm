d8aa26d94359dfce86f4ead453650c416b82bc9b
krmannu <krmannu@my.smccd.edu>
2016-07-14 13:41:16 +0530

    update

--- senddata.php
+++ senddata.php
@@ -1,5 +1,5 @@
 <?php header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Methods: GET);
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 echo json_encode(array('notification'=>array(array('title'=>'title abhi','body'=>'body abhi','icon'=>'icon','tag'=>'tag abhi'))));
 
