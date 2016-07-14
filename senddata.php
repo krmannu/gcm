0f496babf2cc4fd842a08679a28bbafb7da31025
krmannu <krmannu@my.smccd.edu>
2016-07-14 13:45:38 +0530

    yi

--- senddata.php
+++ senddata.php
@@ -2,7 +2,4 @@
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 echo json_encode(array('notification'=>array(array('title'=>'title abhi','body'=>'body abhi','icon'=>'icon','tag'=>'tag abhi'))));
-
-
-
 ?>
