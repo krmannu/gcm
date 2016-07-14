1eec22f8f30e0525852feb8bf72853882523abb4
krmannu <krmannu@my.smccd.edu>
2016-07-14 12:55:03 +0530

    update

--- senddata.php
+++ senddata.php
@@ -1,6 +1,6 @@
 <?php
-
-
+header('content-type: application/json; charset=utf-8');
+header("access-control-allow-origin: *");
 echo json_encode(array('notification'=>array(array('title'=>'title abhi','body'=>'body abhi','icon'=>'icon','tag'=>'tag abhi'))));
 
 
