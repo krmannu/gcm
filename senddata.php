4b607464f6813b9981dad1801d247bd01183edb0
krmannu <krmannu@my.smccd.edu>
2016-07-14 15:10:19 +0530

    update

--- senddata.php
+++ senddata.php
@@ -1,4 +1,4 @@
 <?php header('content-type: application/json; charset=utf-8');
-	  header("access-control-allow-origin: http://localhost/gcm/");
+	  header("access-control-allow-origin: *");
 echo json_encode(array('notification'=>array(array('title'=>'title abhi','body'=>'body abhi','icon'=>'icon','tag'=>'tag abhi'))));
 ?>
